d6452323b96e1d9f7fdfc64f8eb7c6c9daac5de9
Martin Zimandl <martin.zimandl@gmail.com>
2024-08-09 14:20:48 +0200

    handle null across tables

--- scripts/triggers_cnc.sql
+++ scripts/triggers_cnc.sql
@@ -22,10 +22,10 @@ BEGIN
         IF @corpus_metadata_id IS NULL THEN
             INSERT INTO vlo_metadata_corpus (corpus_name) VALUES (@corpus_name);
             INSERT INTO vlo_metadata_common (desc_cs, desc_en, corpus_metadata_id, contact_user_id)
-                VALUES (NEW.description_cs, NEW.description_en, LAST_INSERT_ID(), @contact_user_id);
+                VALUES (COALESCE(NEW.description_cs,''), COALESCE(NEW.description_en,''), LAST_INSERT_ID(), @contact_user_id);
         ELSEIF @skip_vlo_update IS NULL THEN
             SET @skip_corpora_update = 1;
-            UPDATE vlo_metadata_common SET desc_cs = NEW.description_cs, desc_en = NEW.description_en WHERE corpus_metadata_id = @corpus_metadata_id;
+            UPDATE vlo_metadata_common SET desc_cs = COALESCE(NEW.description_cs,''), desc_en = COALESCE(NEW.description_en,'') WHERE corpus_metadata_id = @corpus_metadata_id;
             SET @skip_corpora_update = NULL;
         END IF;
     END IF;
@@ -39,7 +39,7 @@ BEGIN
     IF NEW.type = 'corpus' AND (NOT (NEW.desc_cs <=> OLD.desc_cs) OR NOT (NEW.desc_en <=> OLD.desc_en)) THEN
         IF @skip_corpora_update IS NULL THEN
             SET @skip_vlo_update = 1;
-            UPDATE corpora SET description_cs = NEW.desc_cs, description_en = NEW.desc_en WHERE name = (SELECT corpus_name FROM vlo_metadata_corpus WHERE id = NEW.corpus_metadata_id);
+            UPDATE corpora SET description_cs = NULLIF(NEW.desc_cs,''), description_en = NULLIF(NEW.desc_en,'') WHERE name = (SELECT corpus_name FROM vlo_metadata_corpus WHERE id = NEW.corpus_metadata_id);
             SET @skip_vlo_update = NULL;
         END IF;
     END IF;
