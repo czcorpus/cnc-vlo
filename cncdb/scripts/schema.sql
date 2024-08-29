b080adb10f3c5dfe0f597575f1faea75084c1491
Martin Zimandl <martin.zimandl@gmail.com>
2024-08-29 11:51:51 +0200

    improve table constraints

--- cncdb/scripts/schema.sql
+++ cncdb/scripts/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE vlo_metadata_corpus (
   id int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
   corpus_name varchar(63) NOT NULL,
-  CONSTRAINT vlo_metadata_corpus_corpus_name_fk FOREIGN KEY (corpus_name) REFERENCES kontext_corpus(name) ON DELETE RESTRICT ON UPDATE CASCADE,
+  CONSTRAINT vlo_metadata_corpus_corpus_name_fk FOREIGN KEY (corpus_name) REFERENCES kontext_corpus(name) ON DELETE CASCADE ON UPDATE CASCADE,
   UNIQUE (corpus_name)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 
@@ -28,6 +28,6 @@ CREATE TABLE vlo_metadata_common (
   corpus_metadata_id INT,
   service_metadata_id INT,
   CONSTRAINT vlo_metadata_common_contact_user_id_fk FOREIGN KEY (contact_user_id) REFERENCES kontext_user(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT vlo_metadata_common_corpus_metadata_id_fk FOREIGN KEY (corpus_metadata_id) REFERENCES vlo_metadata_corpus(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT vlo_metadata_common_service_metadata_id_fk FOREIGN KEY (service_metadata_id) REFERENCES vlo_metadata_service(id) ON DELETE RESTRICT ON UPDATE RESTRICT
+  CONSTRAINT vlo_metadata_common_corpus_metadata_id_fk FOREIGN KEY (corpus_metadata_id) REFERENCES vlo_metadata_corpus(id) ON DELETE CASCADE ON UPDATE RESTRICT,
+  CONSTRAINT vlo_metadata_common_service_metadata_id_fk FOREIGN KEY (service_metadata_id) REFERENCES vlo_metadata_service(id) ON DELETE CASCADE ON UPDATE RESTRICT
 ) ENGINE=InnoDB DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
